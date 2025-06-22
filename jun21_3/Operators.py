6f2f47b5b0d609022b3fc87231821d6ec9dacfba
Shivakanya <shivakanyahosur1999@gmail.com>
2025-06-22 20:57:46 +0530

    changes

---
+++ jun21_3/Operators.py
@@ -0,0 +1,62 @@
+age = 65
+age = +65  # unary operator which is adedd to 1 operand
+age2 = -65
+print(age)
+print(age2)
+
+how_many_cars = -1
+print(how_many_cars)
+
+# Airthmetic operator
+print(2 + 2, 2 - 2, 2 * 2, 2 / 2)
+
+# // => Quotient
+# % => Remainder
+
+print(5 // 2)  # 2 modules operator
+print(5 % 2)  # 1
+
+# ** => power operator
+print(2 ** 2)
+print(2 ** 3)
+print(2 ** 4)
+
+# // and /
+print(5 // 2)  # Quotient (int)
+print(5 / 2)  # div
+
+# Compare operator ( True or False)
+print(2 == 2)
+print(2 == 3)
+
+# Not operator
+Is_Shivakanya_Married = False
+print(not Is_Shivakanya_Married)
+
+# Logical Operator => bool
+# >, <, >=, <=
+x, y = 10, 20
+print(x > y, x < y)
+print(x >= y, x <= y)
+
+# OR or And gate
+T = True
+F = False
+print(T or F)
+print(T and F)
+
+result = (x != y)
+print(result)
+
+# divmod function
+q, r = divmod(5, 2)
+print(q, r)  # q = Quotient r= remainder
+
+# Increament ++ Decreament --
+a = 5
+a += 1  # a = a + 1
+print(a)
+a -= 1
+print(a)
+a *= 3
+print(a)
