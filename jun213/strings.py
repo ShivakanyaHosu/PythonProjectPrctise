87251a9e4fafd883cdda00962300675834f2d97d
Shivakanya <shivakanyahosur1999@gmail.com>
2025-06-22 14:32:58 +0530

    strings

---
+++ jun213/strings.py
@@ -0,0 +1,24 @@
+name = "shivakanya"  # bunch of characters
+
+# single quotes or double quotes both are string only
+# no char type in the python
+
+print(len(name))  # length always starts from the 1
+print(name.upper())  # To convert upper case
+print(name.lower())  # To conver lower case
+
+age = "60"
+age1 = 60
+print(type(age))
+print(type(age1))
+
+print(type(int(age)))
+
+# functions to convert => int(), float(), str(), bool()
+
+# concatenation
+print(name + str(1))
+
+f_name = "Shivakanya"
+l_name = "Hosur"
+print(f_name + " " + l_name)
---
+++ jun213/sum sub mul div.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter the num1"))
+num2 = int(input("Enter the num2"))
+
+print("sum is ", num1 + num2)
+print("substraction is ", num1 - num2)
+print("multiplication is ", num1 * num2)
+print(" division is ", num1 / num2)
