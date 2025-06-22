1f3db2b8c5e89669d784dc4ccfd993d15cf40b64
Shivakanya <shivakanyahosur1999@gmail.com>
2025-06-22 14:54:02 +0530

    changes

--- jun21_3/strings.py
+++ jun21_3/strings.py
@@ -22,3 +22,18 @@ print(name + str(1))
 f_name = "Shivakanya"
 l_name = "Hosur"
 print(f_name + " " + l_name)
+
+# \n is for adding new line
+print("shivakanya\nhosur")
+# \t is for adding 1 tab
+print("shivakanya\thosur")
+
+# \b is for 1 backspace ex: it will delete one character
+print("shivakanya\bhosur")
+
+# difference btw single and double character
+# where this 'r' concept will use??
+
+dir = r"C:\shivakanya\n.txt"
+dir1 = r"C:\shivakanya\t.txt"
+print(dir, dir1)
