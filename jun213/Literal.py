f90ea04b3b289c8952af68ab68609698e9aea9e5
Shivakanya <shivakanyahosur1999@gmail.com>
2025-06-22 14:13:22 +0530

    sum

---
+++ jun213/Literal.py
@@ -0,0 +1,9 @@
+age = 65  # 65 is the literal
+print(pow(2, 3))  # power function
+b = abs(-2)  # positive value
+print(b)
+
+# how to take user input
+name = input("enter your name")  # type is always string
+print(" your name is ", name)
+print(type(name))
