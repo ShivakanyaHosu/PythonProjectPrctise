33a6584da7754fa0ee18f9a168787071db38f982
Shivakanya <shivakanyahosur1999@gmail.com>
2025-06-28 21:59:30 +0530

    changes

--- June28/passkeyword.py
+++ June28/passkeyword.py
@@ -3,3 +3,8 @@ for i in range(0, 10, 1):
         print(i)
     else:
         pass  # it is used when syntacally required but no action is needed
+
+print("---------")
+for i in range(101):
+    if i % 2 == 0:  # 0 % 2 =0
+        print(i)
