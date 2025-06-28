29a6ea5cc957338d3feb68b8c9086ccb2ea5488a
Shivakanya <shivakanyahosur1999@gmail.com>
2025-06-28 11:37:43 +0530

    changes

---
+++ jun21_3/Operators2.py
@@ -0,0 +1,11 @@
+# = if age > 18 allowed to vote
+# else not allowed to
+
+age = int(input("enter the age\n"))
+
+if age > 18:
+    print(" eligible for voting")
+else:
+    print(" not eligible for voting")
+
+# print("eligible for voting " if age > 18 : else " not eligible for voting")
