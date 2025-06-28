08e55ef83769a8fc80018372bbe1f935cd3ad127
Shivakanya <shivakanyahosur1999@gmail.com>
2025-06-28 21:22:48 +0530

    changes

---
+++ June28/For loop.py
@@ -0,0 +1,14 @@
+for i in range(1, 10):  # 10 is last-1 , Range is (start, last-1)
+    print(i)
+for i in range(0, 10, 2):  # 10 is last-1 , Range is (start, last-1)
+    print(i)
+for i in range(3, 5):  # 3,4
+    print(i)
+for i in range(1, 10, 2):  # 1 3 5 7 9
+    print(i)
+for i in range(10):  # 0-9
+    print(i)
+for i in range(-10, 10, 2):  # -10, -8 ... 8
+    print(i)
+for i in range(-1, -10, -1):  # -1,-2 ...-9
+    print(i)
