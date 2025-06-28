901e1d89d6ba7e0705a4b0a30132acd4a161a46d
Shivakanya <shivakanyahosur1999@gmail.com>
2025-06-28 12:43:49 +0530

    changes

---
+++ June28/Ifcondition.py
@@ -0,0 +1,20 @@
+# program to go club
+
+age = int(input("enter your age\n"))
+if age >= 21:
+    print(" Allow to go club")
+else:
+    print(" Not allow to go clud")
+
+# Max between 2 numbers
+
+num1 = float(input("Enter the num1\n"))
+num2 = float(input("Enter the num2\n"))
+
+if num1 >= num2:
+    print("num1 is max", num1)
+else:
+    print("num2 is max", num2)
+
+# edge cases num1 =num2
+# user input is string=> abc, ten
